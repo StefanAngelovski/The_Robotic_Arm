3ddec3d00dc1a46a5dd87bddbf46487dbb1be4bc
Tocili, Vasilaki <vasilakitocili@xrite.com>
2024-10-23 22:01:50 +0200

    Clean code refactor
    -Introduced header file with constants in which there are separate sections(general setup, directions for switch case, constants for degrees, steps and finally button ID constant)
    -Refactored the main code with the constants mentioned above, exchanged magic numbers in the switch case with directional constants, exchanged magic numbers in the steps, rotations and directions. Renamed vague and non-readable variables with more meaningful ones, such as rotationalValue, verticalValue etc. Renamed the pins into constants-like to know that they have constant values

---
+++ Constants.h
@@ -0,0 +1,33 @@
+// setup
+#define SERIAL_SPEED 9600
+#define TITLE        "Finki - Robotic arm"
+#define TRUE         "True"
+#define FALSE        "False"
+
+// directions
+#define LEFT     1
+#define RIGHT    2
+#define UP       3
+#define DOWN     4
+#define GRIPPER  5
+#define FORWARD  6
+#define BACKWARD 7
+
+// degrees
+#define ZERO_ROTATIONAL_SERVO 0
+#define MIN_ROTATIONAL_SERVO  50
+#define MAX_ROTATIONAL_SERVO  180
+#define MIN_VERTICAL_SERVO    0
+#define MAX_VERTICAL_SERVO    180
+#define MIN_GRIPPER_SERVO     160
+#define MAX_GRIPPER_SERVO     180
+#define MIN_HORIZONTAL_SERVO  0
+#define MAX_HORIZONTAL_SERVO  180
+
+// steps
+#define ROTATIONAL_STEP 10
+#define VERTICAL_STEP   10
+#define HORIZONTAL_STEP 10
+
+// button ID
+#define BUTTON_ID 10
